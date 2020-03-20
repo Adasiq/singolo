c92b4cbae78f5bad7e7b0a56d8ca3b8031d5f6b6
Anthony Adasik <anthonyadasiq@gmail.com>
2020-03-20 10:03:17 +0300

    feat: DOM task started

---
+++ script.js
@@ -0,0 +1,11 @@
+//=====================MENU BAR=====================//
+
+const menubar = document.getElementsByTagName('body');
+const menu = document.getElementsByClassName('menu');
+menu.querySelectorAll('a');
+
+function changeOn(){
+   menuLink.style.active;
+}
+
+document.addEventListener("click", changeOn());
