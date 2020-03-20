b5f13cdf230f7a952f344d205a638fa2ac12fef2
Anthony Adasik <anthonyadasiq@gmail.com>
2020-03-21 00:25:41 +0300

    fead: header functions

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-//=====================MENU BAR=====================//
+//============================== HEADER ==========================
+let navbar = document.querySelector('nav');
 
-const menubar = document.getElementsByTagName('body');
-const menu = document.getElementsByClassName('menu');
-menu.querySelectorAll('a');
+navbar.addEventListener('click', (event) =>{
+   navbar.querySelectorAll("div > a").forEach( el => el.classList.remove('active'));
+   if (event.target.tagName == 'A') {
+      event.target.classList.add('active');
+   }
+});
 
-function changeOn(){
-   menuLink.style.active;
+document.addEventListener('scroll', OnScroll);
+
+function OnScroll(event){
+   const curPos = window.scrollY;
+   const blocks = document.querySelectorAll('body > div');
+   const link = document.querySelectorAll("nav > div > a");
+
+   blocks.forEach( (el) => {
+      if (el.offsetTop <= curPos && (el.offsetTop + el.offsetHeight) > curPos) {
+         link.forEach((a) => {
+            a.classList.remove('active');
+            if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
+               a.classList.add('active');
+            }
+         });
+      }
+   });
 }
-
-document.addEventListener("click", changeOn());
