2c36a7a74450ab7a6685b1e76a59ea7c9716426d
Anthony Adasik <anthonyadasiq@gmail.com>
2020-03-21 23:16:39 +0300

    feat: dom features added

--- script.js
+++ script.js
@@ -26,3 +26,78 @@ function OnScroll(event){
       }
    });
 }
+//==========================SLIDER=============================
+const right = document.querySelector("div.chevR");
+const left = document.querySelector("div.chevL");
+const slide = document.querySelector("div.slider");
+const main = document.querySelector("#main");
+const slide2 = document.querySelector("div.slider2");
+const right2 = document.querySelector("#main > div.slider2 > div.chevR")
+const left2 = document.querySelector("#main > div.slider2 > div.chevL")
+right.addEventListener('click', (event) =>{
+      console.log('click right');
+      slide.style.display = 'none';
+      slide2.style.display = 'flex';
+      main.style.background = '#648bf0';
+});
+right2.addEventListener('click', (event) =>{
+      console.log('click right');
+      slide2.style.display = 'none';
+      slide.style.display = 'flex';
+});
+left.addEventListener('click', (event) =>{
+      console.log('click left');
+      slide.style.display = 'none';
+      slide2.style.display = 'flex';
+      main.style.background = '#648bf0';
+});
+left2.addEventListener('click', (event) =>{
+      console.log('click left');
+      slide2.style.display = 'none';
+      slide.style.display = 'flex';
+});
+//===================== PHONES ===============================
+
+const hscreen = document.querySelector("#slider > div.horizontal__phone > div > div.screen");
+let hcounter=0;
+hscreen.addEventListener('click', (event) => {
+   console.log('screen');
+   hcounter++;
+   if (hcounter%2 == 0) {
+      hscreen.style.background = "url('assets/Layer 5@1X.png')";
+   } else {
+      hscreen.style.background = 'black';
+   }
+});
+
+
+const vscreen = document.querySelector("#slider > div.vertical__phone > div > div.screen");
+let vcounter = 0;
+vscreen.addEventListener('click', (event) => {
+   console.log('screen');
+   vcounter++;
+   if (vcounter%2 == 0) {
+      vscreen.style.background = "url('assets/Layer 6@1X.png')";
+   } else {
+      vscreen.style.background = 'black';
+   }
+});
+
+//========================== PORTFOLIO =======================
+
+const portfolioNav = document.querySelector("#portfolio");
+portfolioNav.addEventListener('click', (event) =>{
+   event.preventDefault();
+   portfolioNav.querySelectorAll("div.portfolio__nav > a").forEach( el => el.classList.remove('awhite'));
+   if (event.target.tagName == 'A') {
+      event.target.classList.add('awhite');
+   }
+});
+
+var dialog = document.querySelector('dialog');
+document.querySelector('#show').onclick = function() {
+  dialog.show();
+};
+document.querySelector('#close').onclick = function() {
+  dialog.close();
+};
